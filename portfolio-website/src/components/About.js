7ace23e44b47fbcc02337240ebf8c2b4da4be108
vaishnavidevipatil <vaishnavi@factree.ai>
2025-08-19 23:23:41 +0530

    "used marquee"

--- portfolio-website/src/components/About.js
+++ portfolio-website/src/components/About.js
@@ -11,20 +11,9 @@ const About = () => {
                 <div className="about-text">
                   <p><b>
                     Proven Full Stack Developer adept in responsive design, excelled in integrating APIs and enhancing data management. Demonstrated strong problem-solving and team collaboration skills, achieving improvements in functionality and security with a commitment to excellence and innovation.
-                    </b></p>
-          
-           <div className="about-links">
-              <a href="https://www.linkedin.com/in/vaishnavi-devi-patil-711176222" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-              <a href="https://github.com/vaishnavidevipatil" target="_blank" rel="noopener noreferrer">GitHub</a>
-              {/* Add HackerRank link if available */}
-              <a href='https://leetcode.com/u/VAISHNAVIDP/' target="_blank" rel='noopener noreferrer'>LEETCODE</a>
-                <a href='https://www.hackerrank.com/profile/vaishnavidevip17' target="_blank" rel='noopener noreferrer'>HackerRank</a>
-            </div>
-                
+                    </b></p>             
                 </div>
               </div>
-      
-    
     </section>
   );
 };
--- portfolio-website/src/components/Projects.js
+++ portfolio-website/src/components/Projects.js
@@ -1,18 +1,42 @@
 import React from 'react';
-// import './Projects.css';
+
 import project from '../img/blueprint.png';
+import '../styles/project.css';
+import chatbot from '../img/chatbot.png';
+import lstm from '../img/lstm.png';
+
 
 const Projects = () => {
   return (
     <section id="projects" className="projects-section">
-        <img src={project} alt="project icon" className="project-icon" />
-        <h2>Projects</h2>
+        
+        <h2>
+          {/* <img src={project} alt="project icon" className="project-icon" /> */}
+          Projects</h2>
+          <div class="projects-section">
+          {/* Different cards */}
+            <div class="project-card">
+              <img src={chatbot} alt="Project Image" />
+              <h3>AI Chatbot</h3>
+              <p>A lightweight chatbot built using LangChain and GROQ API for lightning-fast performance.</p>
+              <a href='https://github.com/vaishnavidevipatil/langchain'  target="_blank" 
+                rel="noopener noreferrer">
+                View Project
+              </a>
+              <p><b>SKILLS</b>: <u>Python,GenAI, LLMs, and chatbot</u></p>
+            </div>
 
-
+           <div className="project-card">
+            <img src={lstm} alt="Project Image" />
+        <h3>LSTM Model for Stock Market Prediction</h3>
+        <p>
+          Created a prediction model using LSTM for stock market analysis dashboard built with python streamlit integrating Yahoo Finance API.</p>
+        <p><b>SKILLS</b>: <u>Python,Machine Learning TensorFlow, Keras, Data Analysis</u></p>
+      </div> 
       <div className="project-card">
         <h3>Stock Market Visualization Dashboard</h3>
         <p>
-          A live stock market dashboard built with React.js and Node, integrating Yahoo Finance API for data and displaying charts, technical indicators, and market insights.
+         
         </p>
       </div>
       <div className="project-card">
@@ -21,12 +45,8 @@ const Projects = () => {
           Developed a secure authentication system using FastAPI, JWT, and bcrypt for password encryption, enabling secure user login and registration.
         </p>
       </div>
-      <div className="project-card">
-        <h3>LSTM Model for Stock Market Prediction</h3>
-        <p>
-          Created a prediction model using LSTM for stock market analysis, improving accuracy for future predictions and data visualization.
-        </p>
-      </div>
+  
+    </div>
     </section>
   );
 };
